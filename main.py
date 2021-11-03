3e23e8abacdbf0d15d40330e88e06eb0ffd5dc05
Romann37 <mihalkovo62@gmail.com>
2021-11-03 18:31:23 +0300

    дз1 phytonpro

---
+++ main.py
@@ -0,0 +1,9 @@
+from application import salary
+from db import people
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+    print(datetime.today())
